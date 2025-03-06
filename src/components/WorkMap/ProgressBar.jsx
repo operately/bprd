924fb86d8d66a0a7559655dc8ec07e20b7792c06
Marko Anastasov <marko@operately.com>
2025-03-06 12:48:33 +0100

    improve progress bars
    Signed-off-by: Marko Anastasov <marko@operately.com>

--- src/components/WorkMap/ProgressBar.jsx
+++ src/components/WorkMap/ProgressBar.jsx
@@ -8,26 +8,29 @@ export function ProgressBar({ progress, status }) {
   let progressColor;
   switch (status) {
     case "on_track":
-      progressColor = "bg-green-400";
+    case "completed":
+      progressColor = "bg-green-500";
       break;
     case "paused":
+    case "dropped":
       progressColor = "bg-gray-400";
       break;
     case "caution":
-      progressColor = "bg-yellow-400";
+      progressColor = "bg-amber-500";
       break;
     case "issue":
-      progressColor = "bg-red-400";
+    case "failed":
+      progressColor = "bg-red-500";
       break;
     case "pending":
-      progressColor = "bg-blue-400";
+      progressColor = "bg-blue-500";
       break;
     default:
       progressColor = "bg-gray-400";
   }
   
   return (
-    <div className="w-full h-2 bg-gray-100 rounded-full overflow-hidden">
+    <div className="w-full h-3 bg-gray-200 rounded-full overflow-hidden">
       <div 
         className={`h-full rounded-full ${progressColor}`}
         style={{ width: progressWidth }}
