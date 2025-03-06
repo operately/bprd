77777b5be249a44133a60ae836b3520c511b29b9
Marko Anastasov <marko@operately.com>
2025-03-06 13:23:44 +0100

    stronger header
    Signed-off-by: Marko Anastasov <marko@operately.com>

--- src/components/WorkMap/WorkMapTable.jsx
+++ src/components/WorkMap/WorkMapTable.jsx
@@ -7,20 +7,20 @@ export default function WorkMapTable() {
     <div className="w-full">
       <table className="w-full min-w-[1000px] table-auto">
         <thead>
-          <tr className="border-b border-surface-outline text-content-dimmed text-sm">
-            <th className="text-left py-3 px-4 font-medium">Name</th>
-            <th className="text-left py-3 px-4 font-medium w-[130px]">
+          <tr className="border-b-2 border-surface-outline bg-surface-dimmed text-content-base text-sm sticky top-0">
+            <th className="text-left py-3.5 px-4 font-semibold">Name</th>
+            <th className="text-left py-3.5 px-4 font-semibold w-[130px]">
               Status
             </th>
-            <th className="text-left py-3 px-4 font-medium w-[130px]">
+            <th className="text-left py-3.5 px-4 font-semibold w-[130px]">
               Progress
             </th>
-            <th className="text-left py-3 px-4 font-medium w-[100px]">Space</th>
-            <th className="text-left py-3 px-4 font-medium w-[120px]">Owner</th>
-            <th className="text-left py-3 px-4 font-medium w-[100px]">
+            <th className="text-left py-3.5 px-4 font-semibold w-[100px]">Space</th>
+            <th className="text-left py-3.5 px-4 font-semibold w-[120px]">Owner</th>
+            <th className="text-left py-3.5 px-4 font-semibold w-[100px]">
               Deadline
             </th>
-            <th className="text-left py-3 px-4 font-medium w-[300px]">
+            <th className="text-left py-3.5 px-4 font-semibold w-[300px]">
               Next step
             </th>
           </tr>
